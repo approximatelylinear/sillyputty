b908f120bccea914a0805dffaee198a115397b78
MJ Berends <mj@dose.com>
2016-06-12 16:38:10 -0500

    Add save_model and load_model routines to model.py

--- model_tools/model.py
+++ model_tools/model.py
@@ -15,6 +15,7 @@ except ImportError:
 import pdb
 
 #   3rd party
+import yaml
 import cloudpickle  # For function serialization
 from sklearn.pipeline import make_pipeline as sk_make_pipeline
 
@@ -22,201 +23,30 @@ from sklearn.pipeline import make_pipeline as sk_make_pipeline
 from .exceptions import *
 from .util.util import md5_hex, import_func
 from .util import odo_util
-from .util.odo_util import (
-    odo, convert as odo_convert,
-    resource as odo_resource, append as odo_append)
+from .util.odo_util import odo, odo_convert, odo_resource, odo_append
+from .model_persistence import EstimatorPickler
 
 try:
     THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 except NameError:
     THIS_DIR = os.path.abspath(os.getcwd())
+PATH_DATA = os.getenv('PATH_MODEL_DATA') or os.path.join(THIS_DIR, 'data')
 
 LOGGER = logging.getLogger(__name__)
 
 
-class ModelFactory(object):
-    _default_model_wrapper = Model
-
-    def __init__(self, namespace=None, default_model_wrapper=None, *args, **kwargs):
-        self.namespace = namespace or {}
-        self.default_model_wrapper = self._default_model_wrapper if default_model_wrapper is None else default_model_wrapper
-
-    def _init_config(self):
-        #   TBD
-        raise NotImplementedError
-
-    def _load_attribute(self, info, namespace):
-        """
-        Load a class (or anything else really) given its name or location in package-space.
-
-        TBD: Use multiple dispatch for this?
-        """
-        if isinstance(info, basestring):
-            #   We just have the name of the function
-            cls = namespace.get(info)
-            if cls is None:
-                #   Try to import as an attribute of the caller's module name
-                cls = import_func(
-                    module=namespace['__meta']['caller__name__'],
-                    func=info)
-        elif isinstance(info, dict):
-            cls = import_func(**info)
-        else:
-            cls = None
-        return cls
-
-    def _init_submodels(self, namespace=None):
-        """
-        Create submodels
-        """
-        config = self.config
-
-        submodels =
-        return submodels
-
-    def _init_model(self, namespace=None):
-        """
-        Create this model by instantiating the model callable with the parameters.
-        """
-        if globals_ is None:
-            globals_ = globals()
-        config = self.config
-        submodels = self.submodels
-        parameters = config.get('parameters') or {}
-        model_callable = self._load_model_callable(config, globals_=globals_)
-        model = None
-        if not model_callable:
-            if submodels is not None and len(submodels) == 1:
-                submodel = submodels[0]
-                if submodel is not None:
-                    model = submodel
-                # else:
-                    # raise Exception(
-                    #     "Could not find a model callable for [ {} ]".format(
-                    #         self.__class__.__name__))
-        else:
-            model = model_callable(**parameters)
-        return model
-
-    def _make_model(self, config, namespace):
-        params = {
-            'config_str': yaml.dump(config),
-            'name': config.get('name'),
-            'namespace': namespace,
-            'parameters': config.get('parameters'),
-            'model_callable': self._load_model_callable(config, namespace),
-            'submodels': self._load_submodel_callables(config, namespace),
-            'path_model': (config.get('persistence') or {}).get('model'),
-            'path_data': (config.get('persistence') or {}).get('data'),
-        }
-        wrapper_cls = self._load_model_wrapper(config, namespace=namespace)
-        return wrapper_cls(**params)
-
-    def _load_model_wrapper(self, config, namespace):
-        info = config.get('wrapper_class')
-        return info if isinstance(info, callable) else (self.default_model_wrapper if info is None else self._load_attribute(info, namespace))
-
-    def _load_submodels(self, config, namespace):
-        submodel_configs = config.get('model')
-        return (
-            [ self._make_model(v, namespace) for v in submodel_configs ]
-            if submodel_configs else None)
-
-    def _load_model_callable(self, config, namespace):
-        info = config.get('model_callable')
-        if info is not None:
-            model_callable = info if isinstance(info, callable) else self._load_attribute(info, namespace)
-            assert callable(model_callable)
-        else:
-            model_callable = None
-        return model_callable
-        #   Is there a place where I need this?
-        # return getattr(self, '_model_callable', None) or load_model_callable(self.config, namespace)
-
-    def __call__(self, config, namespace=None):
-        if namespace is None:
-            namespace = self.namespace or {}
-        namespace = copy.copy(namespace)
-        #   Keep track of the caller's module name to use as a fallback namespace
-        namespace.setdefault('__meta', {})['caller__name__'] = inspect.stack()[1][0].f_globals['__name__']
-        return self._make_model(config, namespace)
+def save_model(model_obj, path):
+    with open(path, 'wb') as f_out:
+        cloudpickle.dump(model_obj, path)
 
 
-#   Instantiate callable that returns model instances
-make_model = ModelFactory()
+def load_model(path):
+    with open(path, 'rb') as f_in:
+        return  pickle.load(f_in)
 
 
 
-class Persister(object):
-    _persist_attrs = None
 
-    def __init__(self, *args, **kwargs):
-        super(Persister, self).__init__()
-
-    def fltr_attrs(self):
-        if self._persist_attrs is not None:
-            obj = {k: getattr(self, k, None) for k, v in self._persist_attrs}
-        else:
-            fltr = lambda x: not (x[0].startswith('__') or inspect.isroutine(x[1]))
-            attrs = inspect.getmembers(self)
-            obj = dict(filter(fltr, attrs))
-        return obj
-
-    def _save(self, obj):
-        raise NotImplementedError
-
-    def _load(self):
-        raise NotImplementedError
-
-    def save(self):
-        obj = self.fltr_attrs()
-        self._save(obj)
-        return obj
-
-    def load(self):
-        obj = self._load()
-        for k, v in obj.iteritems():
-            setattr(self, k, v)
-        return self
-
-
-class PicklerMixin(Persister):
-    def __init__(self, path_model, *args, **kwargs):
-        super(PicklerMixin, self).__init__(*args, **kwargs)
-        self.path_model = path_model
-
-    def _save(self, obj):
-        with open(self.path_model, 'wb') as f_out:
-            cloudpickle.dump(obj, f_out)
-
-    def _load(self):
-        if os.path.exists(self.path_model):
-            with open(self.path_model, 'rb') as f_in:
-                obj = pickle.load(f_in)
-                return obj
-
-
-class S3Mixin(Persister):
-    import boto3
-
-    def __init__(self, path_model, *args, **kwargs):
-        super(S3Mixin, self).__init__(*args, **kwargs)
-        self.path_model = path_model
-        client = self.boto3.client('s3')
-        self.client = client
-
-
-class SQSMixin(Persister):
-    import boto3
-
-    def __init__(self, path_model, *args, **kwargs):
-        super(SQSMixin, self).__init__(*args, **kwargs)
-        self.path_model = path_model
-        client = self.boto3.client('sqs')
-        self.client = client
-
-
-# class Model(Configurer):
 class Model(object):
     """
     Wrapper for a model that handles configuration and provides entrypoints.  It implements the scikit-learn model interface.
@@ -276,12 +106,12 @@ class Model(object):
             self.config_str = config_str
             self.path_model = path_model
             self.path_data = path_data
+            self.submodels = submodels
             self.parameters = self._init_parameters(copy.deepcopy(parameters) or {})
             self.model = self._init_model()
             self.id = self._init_id()
 
     def _init_parameters(self, parameters):
-        config = self.config
         submodels = self.submodels
         parameters = {}
         parameters[self.name] = parameters
@@ -318,19 +148,9 @@ class Model(object):
                 if submodel is not None:
                     #   TBD: Should this be `submodel` or `submodel.model`?
                     model = submodel.model
-                # else:
-                    # raise Exception(
-                    #     "Could not find a model callable for [ {} ]".format(
-                    #         self.__class__.__name__))
         else:
             model = model_callable(**parameters)
         return model
-
-    def save(self):
-        return self.persister.save()
-
-    def load(self):
-        return self.persister.load()
 
     def fit(self, X, **kwargs):
         """
@@ -487,3 +307,83 @@ class SKPipeliner(Model):
         else:
             model = model_callable(*[sm.model for sm in submodels])
         return model
+
+
+class ModelFactory(object):
+    _default_model_wrapper = Model
+
+    def __init__(self, namespace=None, default_model_wrapper=None, *args, **kwargs):
+        self.namespace = namespace or {}
+        self.default_model_wrapper = self._default_model_wrapper if default_model_wrapper is None else default_model_wrapper
+
+    def _init_config(self):
+        #   TBD
+        raise NotImplementedError
+
+    def _load_attribute(self, info, namespace):
+        """
+        Load a class (or anything else really) given its name or location in package-space.
+
+        TBD: Use multiple dispatch for this?
+        """
+        if isinstance(info, basestring):
+            #   We just have the name of the function
+            cls = namespace.get(info)
+            if cls is None:
+                #   Try to import as an attribute of the caller's module name
+                cls = import_func(
+                    module=namespace['__meta']['caller__name__'],
+                    func=info)
+        elif isinstance(info, dict):
+            cls = import_func(**info)
+        else:
+            cls = None
+        return cls
+
+    def _make_model(self, config, namespace):
+        params = {
+            'config_str': yaml.dump(config),
+            'name': config.get('name'),
+            'namespace': namespace,
+            'parameters': config.get('parameters'),
+            'model_callable': self._load_model_callable(config, namespace),
+            'submodels': self._load_submodels(config, namespace),
+            'path_model': (config.get('persistence') or {}).get('model'),
+            'path_data': (config.get('persistence') or {}).get('data'),
+        }
+        wrapper_cls = self._load_model_wrapper(config, namespace=namespace)
+        return wrapper_cls(**params)
+
+    def _load_model_wrapper(self, config, namespace):
+        info = config.get('wrapper_class')
+        return info if callable(info) else (self.default_model_wrapper if info is None else self._load_attribute(info, namespace))
+
+    def _load_submodels(self, config, namespace):
+        submodel_configs = config.get('model')
+        return (
+            [ self._make_model(v, namespace) for v in submodel_configs ]
+            if submodel_configs else None)
+
+    def _load_model_callable(self, config, namespace):
+        info = config.get('model_callable')
+        if info is not None:
+            model_callable = info if callable(info) else self._load_attribute(info, namespace)
+            assert callable(model_callable)
+        else:
+            model_callable = None
+        return model_callable
+
+    def __call__(self, config, namespace=None):
+        if namespace is None:
+            namespace = self.namespace or {}
+        namespace = copy.copy(namespace)
+        #   Keep track of the caller's module name to use as a fallback namespace
+        namespace.setdefault('__meta', {})['caller__name__'] = inspect.stack()[1][0].f_globals['__name__']
+        return self._make_model(config, namespace)
+
+
+#   Instantiate callable that returns model instances
+make_model = ModelFactory()
+
+
+
